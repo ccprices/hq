ce755f5091c8471d567368d58af38c95430f2728
Sandd <87068650+ccprices@users.noreply.github.com>
2024-07-21 00:11:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def home():
     return "Hi there, Space QBee!"
+
+@app.route('/about')
+def about():
+    return "This is the about page."
 
 
 if __name__ == '__main__':
